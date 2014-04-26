14088125025087b5ff1f16540a5a5583b3d12f9a
Bear <timfanda35@gmail.com>
2014-04-26 20:28:44 +0800

    Initial Commit

---
+++ HelloBindings/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  HelloBindings
+//
+//  Created by Bear on 2014/4/26.
+//  Copyright (c) 2014年 TakoBear. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
